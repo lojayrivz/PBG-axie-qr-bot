cdff424740d1609a3ee8c2cc97b29a2e169315ea
ikr0w <gkrow@protonmail.com>
2021-12-17 19:46:53 +0800

    Status Customization

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
-const { guild_id } = require('../config.json')
+const { guild_id, client_status } = require('../config.json')
 
 module.exports = async (client) => {
-    client.user.setActivity(`Axie Infinity`, { type: 'PLAYING' })
+    client.user.setActivity(client_status.description, { type: client_status.type })
 
     const slashCommandsConfig = client.slashCommands.map(command => command.config)
     // GLOBAL SLASH COMMANDS - Global application commands will be available in all the guilds your application has the "applications.commands" scope authorized, as well as in DMs.
